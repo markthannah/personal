96e1772bc826c53246526c08f9df67460b016ad4
Mark Hannah <hannahm7@msn.com>
2016-12-10 22:36:40 -0500

    added columns to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161208022538) do
+ActiveRecord::Schema.define(version: 20161210210129) do
 
   create_table "posts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "title"
+    t.text     "description"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
